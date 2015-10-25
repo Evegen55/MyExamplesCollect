9fdce8d225d5de90abbf49798b1c127a6de7710e
Johnn <Johnn@192.168.0.101>
2015-10-25 15:27:34 +0300

    add a Readme file

--- src/other_examples/MyBuiltInSortTest.java
+++ src/other_examples/MyBuiltInSortTest.java
@@ -16,10 +16,28 @@
  */
 package other_examples;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author Johnn
  */
 public class MyBuiltInSortTest {
+    
+   public static void main (String[] args) {
+       Random random = new Random();
+       List<Integer> numsToSort = new ArrayList();
+       
+       for (int i = 0; i<5; i++) {
+       numsToSort.add(random.nextInt(100));
+       }
+       
+       Collections.sort(numsToSort);
+       System.out.println("New array after builtin sort: " + numsToSort.toString());
+   
+   }
     
 }
