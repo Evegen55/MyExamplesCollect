162757502a0b245450a513432f9de7f3239772b8
Evegen <demerbrise@gmail.com>
2015-10-29 12:20:25 +0300

    add a package for experimenting with Groovy langauge

---
+++ src/for_groovy/NewGroovyScript.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 Lartsev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package for_groovy
+
+def name='Lartsev'
+
+println "Hello $name!"
+
+println 6.multiply(11).minus(2)
+
+println 6.multiply(11.minus(2))
+
+println (!false || true) && (false || true)
+
+println  "Hello".size()
+
+// println  ['H', 'e', 'l', 'l', 'o'].asBoolean()
+
+// println  ['H', 'e', 'l', 'l', 'o'].reverse()
+
+// println  ['H', 'e', 'l', 'l', 'o'].last()
+
+// println  ['H', 'e', 'l', 'l', 'o'].tail()
+
+// println  ['H', 'e', 'l', 'l', 'o'].head()
+
+println  "1 + 2 == ${1 + 2}"
