7348f67d0548bcf7768f55cef87e312e678528c6
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-07-24 14:19:34 +0300

    just fun with lambdas and threads

--- src/other_examples/JavaLambdas.java
+++ src/other_examples/JavaLambdas.java
@@ -7,39 +7,58 @@ package other_examples;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.Consumer;
+import static java.util.stream.Collectors.toList;
 
 /**
  *
  * @author Evgenn
  */
 public class JavaLambdas {
-
+    private Future f;
+    
     /**
      * 
-     * @param args the command line arguments
      */
+    public void inThread() {
+        Runnable r = () -> System.out.println("inThread() " + Thread.currentThread().getName());
+        Thread t = new Thread(r);
+        t.start();
+    }
     
-    public static void main(String[] args) {
-        final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", 
+    /**
+     * 
+     */
+    public void inExecutorService() {
+        ExecutorService e = Executors.newCachedThreadPool();
+        Runnable r = () -> System.out.println("inExecutorService() " + Thread.currentThread().getName());
+        f = e.submit(r);
+        //f.cancel(true);
+    }
+    
+    public void lambdasInMainThread() {
+        final List<String> friends = Arrays.asList("Brian", "Nate", "Neal",
                 "Raju", "Sara", "Scott");
         
         System.out.println("1st way");
         for(int i = 0; i < friends.size(); i++) {
             System.out.println(friends.get(i));
-            }
+        }
         
         System.out.println("2nd way");
         for(String name : friends) {
             System.out.println(name);
-            }
+        }
         
         System.out.println("3rd way");
         friends.forEach(new Consumer<String>() {
             public void accept(final String name) {
                 System.out.println(name);
-                }
-            });
+            }
+        });
         
         System.out.println("4rd way - лямбда - выражение");
         friends.forEach((final String name) -> 
@@ -48,8 +67,16 @@ public class JavaLambdas {
         System.out.println("5th way - лямбда - выражение");
         friends.forEach(System.out::println);
         
-        System.out.println("5th way - лямбда - выражение");
+        System.out.println("6th way - Streams API");
         friends.stream().forEach(System.out::println);
+        
+        System.out.println("7th way - Streams API - map(), filter()");
+        friends
+                .stream()
+                .filter(friend -> friend.equals("Brian"))
+                //.collect(toList())
+                .forEach(System.out::println);
+        
         
         /*List<String> vitamins = new ArrayList<>();
         vitamins.add("A");
@@ -62,8 +89,20 @@ public class JavaLambdas {
         System.out.println(vitamins);
         vitamins.add(1,"D");
         System.out.println(vitamins);
-                */
-        
-        }
+        */
+    }
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        JavaLambdas jl = new JavaLambdas();
+        //jl.inThread();
+        //jl.inExecutorService();
+        jl.lambdasInMainThread();
+    }
+
+    
     
 }
